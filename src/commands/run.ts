b69e3510c9f35292814b7f88f304ff6c3033385f
Michael Palmer <michaelpalmer53@gmail.com>
2025-08-30 09:26:03 +0100

    updates run command to run actions or pipelines

--- src/commands/run.ts
+++ src/commands/run.ts
@@ -1,17 +1,11 @@
 import {Args, Command, Flags} from '@oclif/core'
 import {join} from 'path'
-import {testActionFn} from '../@core/actions/test.action'
-import {runnerFn, timedRunnerFn} from '../@core/@shared/runner'
-import {RunFn} from '../@core/@shared/types/runnable.types'
+import {timedRunnerFn} from '../@core/@shared/runner'
 import {pipes} from '../@core/pipelines/pipelines.util'
+import {readJsonSync} from 'fs-extra'
 
 export default class Run extends Command {
   static override args = {
-    action: Args.string({
-      name: 'CLI action to run',
-      options: ['generate-key'],
-      required: true,
-    }),
     function: Args.string({
       description: 'function to run',
       required: true,
@@ -31,24 +25,55 @@ export default class Run extends Command {
     force: Flags.boolean({char: 'f'}),
     // flag with a value (-n, --name=VALUE)
     name: Flags.string({char: 'n', description: 'name to print'}),
+    data: Flags.string({
+      char: 'd',
+      description: 'data file to use (to use a path, prefix with @)',
+      exists: true,
+      required: false,
+      parse: (input) => {
+        if (input.charAt(0) === '@') {
+          const data = readJsonSync(input.slice(1))
+          return data
+        }
+
+        try {
+          return JSON.parse(input)
+        } catch (_) {
+          return input
+        }
+      },
+    }),
   }
 
   public async run(): Promise<void> {
     const {args, flags} = await this.parse(Run)
 
-    const action = args.action as any
+    if (Array.isArray(args.function)) {
+      // handle pipeline
+      const fns = args.function.map((item: any) => {
+        return async (context: any) => {
+          const result = await timedRunnerFn(flags.data ?? {data: 'some data to hash'}, item.fn, {
+            mode: 'isolated',
+            max: item.retries || 3,
+            timeout: item.timeout || 2000,
+          } as any)
 
-    const pipeFn = async (context: any) => {
-      const result = await timedRunnerFn(
-        {data: 'payload'},
-        args.function as any,
-        {mode: 'isolated', max: 3, timeout: 2000} as any,
-      )
+          return context.next({data: result})
+        }
+      })
 
-      return context.next({data: result})
+      const pipeline = pipes(...fns)
+      pipeline.config.timeout = 30000
+
+      const ctx = await pipeline.run(flags.data as any)
+      console.log(ctx) // <- eventually store this
+      return
     }
 
-    const pipeline = pipes(pipeFn)
-    await pipeline.run()
+    const result = await timedRunnerFn(flags.data ?? {data: 'some data to hash'}, args.function as any, {
+      mode: 'isolated',
+      retries: 3,
+      timeout: 2000,
+    })
   }
 }
