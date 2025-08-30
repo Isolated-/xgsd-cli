f36c73f16568b739e2d749ed49c2f813e8199514
Michael Palmer <michaelpalmer53@gmail.com>
2025-08-30 15:12:46 +0100

    improves pipeline interface with new orchestrate method (vs old run() which is removed)

--- src/@core/pipelines/pipeline.concrete.ts
+++ src/@core/pipelines/pipeline.concrete.ts
@@ -1,149 +1,105 @@
 import {config} from 'process'
-import {PipeFn, PipelineConfig, PipelineState, PipelineStep, SourceData} from '../@types/pipeline.types'
+import {PipelineConfig, PipelineMode, PipelineState, SourceData} from '../@types/pipeline.types'
 import {IPipeline} from './interfaces/pipeline.interfaces'
 import {getDefaultPipelineConfig} from './pipelines.util'
-import {timedRunnerFn as runnerFn} from '../@shared/runner'
+import {timedRunnerFn} from '../@shared/runner'
 import {debug} from '../util/debug.util'
+import {RunFn} from '../@shared/types/runnable.types'
 
-export const runPipelineStep = async <T extends SourceData>(
+/**
+ *  Orchestrates a single step in the pipeline.
+ *  @param config - The pipeline configuration.
+ *  @param {RunFn} fn - The function to run for this step.
+ *  @param {number} index - The index of the step in the pipeline.
+ *  @note state is mutated by this function, so use with caution.
+ */
+const orchestrateStep = async <T extends SourceData = SourceData>(
   config: PipelineConfig<T>,
-  step: PipelineStep<T>,
+  fn: RunFn<T, T>,
   index: number,
-): Promise<PipelineStep<T> | null> => {
-  const next = async (data?: any | null, errors?: Error | Error[]) => {
-    let outputData = data || config.input
+): Promise<void> => {
+  const previousOutput = config.steps[index - 1]?.output ?? config.input
 
-    if (data instanceof Promise) {
-      outputData = await data
-    }
-
-    if (outputData) {
-      config.output = {...outputData, ...config.output}
-    }
-
-    config.input = config.output
-
-    config.steps[index].run = outputData
-    config.steps[index].state = PipelineState.Succeeded
-
-    config.runs.push(outputData as any)
-    return config.steps[index + 1] ?? null
+  let data = config.input
+  if (config.mode === PipelineMode.Chained) {
+    data = previousOutput as T
   }
 
-  const result = await runnerFn(
-    {
-      ...config,
-      previous: config.steps[index - 1] ?? null,
-      next,
-    },
-    step.pipe as PipeFn,
-    {
-      retries: config.stopOnError ? 1 : config.max,
-      attempt: config.steps[index].attempt ?? 0,
-      timeout: config.timeout,
-      onAttempt(attempt, error, cancel) {
-        debug(`function is currently failing, attempt: ${attempt}`, runPipelineStep.name, step.pipe.name || 'usercode')
+  const result = await timedRunnerFn(data, fn, {
+    mode: 'isolated',
+    retries: config.stopOnError ? 1 : config.max,
+    timeout: config.timeout,
+    delay: config.delay ?? ((attempt) => attempt + 100),
+  })
 
-        config.errors.push(error)
-
-        // update step
-        config.steps[index].state = PipelineState.Retrying
-        config.steps[index].attempt = attempt + 1
-        config.steps[index].retries = (config.steps[index].retries ?? 0) + 1
-
-        if (config.stopOnError) {
-          cancel()
-        }
-      },
-    },
-  )
-
-  config.retries = config.retries + (config.steps[index].retries ?? 0)
-
-  if (result.error) {
-    config.steps[index].state = PipelineState.Failed
-    debug(`(error) ${step.pipe.name} failed with error: ${result.error.message}`, runPipelineStep.name)
-    return config.steps[index + 1] ?? null
-  }
-
-  debug(`step finished with state: ${config.steps[index].state}`, runPipelineStep.name, step.pipe.name)
-
-  return result.data ?? null
+  // update step config
+  config.steps[index].errorMessage = result.errors && result.errors.length > 0 ? result.errors[0].message : null
+  config.steps[index].errors = result.errors || []
+  config.steps[index].input = data!
+  config.steps[index].run = result.data ?? null
+  config.steps[index].output = result.data ?? null
+  config.steps[index].state = result.errors && result.errors.length > 0 ? PipelineState.Failed : PipelineState.Succeeded
+  config.steps[index].attempt = result.retries
 }
 
-export const runPipeline = async <T extends SourceData>(initial: PipelineConfig<T>): Promise<PipelineConfig<T>> => {
-  let current: PipelineStep<T> | null = initial.steps[0] ?? null
-  let index = 0
-
-  debug(
-    `pipeline starting up, number of steps: ${initial.steps.length}, current step: ${index + 1}`,
-    runPipelineStep.name,
-    'pipeline',
-  )
-
-  const start = performance.now()
-
-  while (current && current.state !== PipelineState.Failed && current.state !== PipelineState.Succeeded) {
-    // keep this sanity check here
-    if (!current) break
-
-    debug(`executing step ${current.pipe.name} (${index + 1}) now...`, runPipeline.name, 'pipeline')
-
-    current = await runPipelineStep(initial, current, index)
-
-    debug(
-      `finished step ${index + 1}, next step: ${current?.pipe.name ?? 'no next step'}`,
-      runPipeline.name,
-      'pipeline',
-    )
-
-    if (initial.steps[index].state === PipelineState.Failed && initial.stopOnError) {
-      break
+/**
+ *  Orchestrates the pipeline.
+ *  @param config - The pipeline configuration.
+ *  @returns The updated pipeline configuration.
+ *  @note state is mutated by this function, so use with caution.
+ */
+const orchestrate = async <T extends SourceData = SourceData>(
+  config: PipelineConfig<T>,
+): Promise<PipelineConfig<T>> => {
+  if (config.mode !== PipelineMode.Async) {
+    // If not async, run steps synchronously
+    for (const [idx, step] of config.steps.entries()) {
+      await orchestrateStep(config, step.fn!, idx)
     }
-
-    index++
+  } else {
+    await Promise.all(config.steps.map((step, idx) => orchestrateStep(config, step.fn!, idx)))
   }
 
-  // determine final state
-  initial.state = PipelineState.Succeeded
+  // perform aggregations
+  config.output = config.steps.reduce((acc: any, step) => ({...acc, ...step.run}), {})
+  config.errors = config.steps.reduce((acc: any, step) => [...acc, ...step.errors!], [])
+  config.retries = config.steps.reduce((acc: number, step) => acc + (step.attempt ?? 0), 0)
+  config.state = config.errors.length > 0 ? PipelineState.Failed : PipelineState.Completed
+  config.runs = config.steps.map((step) => step.run).filter(Boolean) as any[]
 
-  const failedSteps = initial.steps.filter((c) => c.state === PipelineState.Failed)
-  const successfulSteps = initial.steps.filter((c) => c.state === PipelineState.Succeeded)
-
-  const end = performance.now()
-  debug(
-    `pipeline has been completed, completed ${initial.steps.length} steps in ${(end - start).toFixed(2)}ms`,
-    runPipeline.name,
-    'pipeline',
-  )
-
-  // succeeded = no failed steps
-  if (failedSteps.length === 0) {
-    initial.state = PipelineState.Succeeded
-    return initial
+  // for chained mode, input goes through to output
+  if (config.mode === PipelineMode.Chained) {
+    config.output = config.steps[config.steps.length - 1].output
   }
 
-  // complete = some succeeded steps and some failed steps
-  if (failedSteps.length >= 0 && successfulSteps.length >= 0) {
-    initial.state = PipelineState.Completed
-    return initial
-  }
-
-  initial.state = PipelineState.Failed
-  return initial
+  return config
 }
 
 export class Pipeline<T extends SourceData = SourceData> implements IPipeline<T> {
   constructor(public config: PipelineConfig<T>) {}
 
-  async run(input: T): Promise<PipelineConfig<T>> {
-    let config = getDefaultPipelineConfig<T>({
+  /**
+   *  The main entry point into the Pipeline. Accepts input and an array of functions to run.
+   *  @param input
+   *  @param fns
+   *  @returns {PipelineConfig<T>} the updated pipeline configuration.
+   */
+  async orchestrate(input: T | null, ...fns: RunFn<T, T>[]): Promise<PipelineConfig<T>> {
+    const config = getDefaultPipelineConfig<T>({
       ...this.config,
-      input,
+      steps: fns.map((c) => ({
+        input: null,
+        output: null,
+        run: null,
+        fn: c,
+        state: PipelineState.Pending,
+        errors: [],
+      })),
+      input: input as T,
       output: null,
       state: PipelineState.Running,
     })
 
-    return runPipeline<T>(config)
+    return orchestrate(config)
   }
 }
