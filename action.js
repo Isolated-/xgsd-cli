2b728340881ae8403f666238ed3a1493184f757c
Michael Palmer <michaelpalmer53@gmail.com>
2025-08-30 09:26:15 +0100

    misc generated changes

--- action.js
+++
@@ -1,9 +0,0 @@
-const myAction = async (context) => {
-  const crypto = require('crypto')
-
-  const data = context.data
-  const hash = crypto.createHash('sha256').update(data).digest('hex')
-
-  return hash
-}
-module.exports = myAction
