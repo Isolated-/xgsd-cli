0b49fac86d593a27b7a6122aa6fbec83a08d3496
Michael Palmer <michaelpalmer53@gmail.com>
2025-09-04 23:33:00 +0100

    update runStep to resolve env var data before passing it to refactored ProcessManager

--- src/@core/@shared/workflow.process.ts
+++ src/@core/@shared/workflow.process.ts
@@ -4,7 +4,7 @@ import * as ms from 'ms'
 import {WrappedError} from './runner'
 import * as lodash from 'lodash'
 import {resolveStepData, resolveStepTemplates} from './runner.process'
-import {fork} from 'child_process'
+import {fork, ForkOptions} from 'child_process'
 import {WorkflowContext} from './context.builder'
 import {join} from 'path'
 import {WorkflowEvent} from '../workflows/workflow.events'
@@ -54,112 +54,133 @@ export class WorkflowError extends Error {
   }
 }
 
-async function runStep(idx: number, step: PipelineStep, context: WorkflowContext) {
-  return new Promise<{step: any; fatal: boolean; errors: any[]}>((resolve, reject) => {
-    const stepProcess = fork(join(__dirname, 'workflow.step-process.js'), {
-      stdio: ['pipe', 'pipe', 'pipe', 'ipc'],
+export class ProcessManager {
+  process: any
+  startedAt: number
+
+  constructor(
+    public step: PipelineStep,
+    public context: WorkflowContext,
+    public path: string,
+    public timeoutMs?: number,
+  ) {
+    this.startedAt = Date.now()
+  }
+
+  fork(args: ForkOptions[] = []) {
+    this.process = fork(this.path, {
+      stdio: args.length ? (['pipe', 'pipe', 'pipe', 'ipc'] as any) : (['pipe', 'pipe', 'pipe', 'ipc'] as any),
+      env: {
+        GSD_WORKFLOW_ID: this.context.id,
+        GSD_WORKFLOW_HASH: this.context.hash,
+        ...this.step.env,
+      },
+      execArgv: ['--max-old-space-size=256'],
     })
 
-    const startedAt = new Date().toISOString()
-
-    let timeout = step.options?.timeout
-    if (typeof timeout === 'string') {
-      timeout = ms(timeout as ms.StringValue)
-    }
-
-    const timerHandler = () => {
-      stepProcess.kill()
-      const error = new WorkflowError('hard timeout limit reached', WorkflowErrorCode.HardTimeout)
-
-      const updated = {
-        ...step,
-        index: idx,
-        startedAt: startedAt,
-        endedAt: new Date().toISOString(),
-        duration: new Date().getTime() - new Date(startedAt).getTime(),
-        state: PipelineState.Failed,
-        error,
-        errors: [error],
-      }
-
-      event(WorkflowEvent.StepFailed, {
-        step: updated,
-        attempt: {error: updated.error, retries: 0, nextMs: 0, finalAttempt: true},
-      })
-
-      resolve({
-        step: updated,
-        fatal: true,
-        errors: [],
-      })
-    }
-
-    let timer = setTimeout(() => {}, timeout! * 10000)
-    stepProcess.on('message', (msg: any) => {
-      switch (msg.type) {
-        case 'CHILD:EVENT':
-          event(msg.event, msg.payload)
-          if (msg.event === WorkflowEvent.StepRetry) {
-            clearTimeout(timer)
-            // adds nextMs + original timeout to give some buffer time on retries
-            // leave timeout in place so hard timeout doesn't kick it
-            timer = setTimeout(timerHandler, timeout! + msg.payload.attempt.nextMs + 500)
-            return
-          }
-
-          // adds to give some buffer time on retries
-          if (msg.event === WorkflowEvent.StepStarted) {
-            clearTimeout(timer)
-            timer = setTimeout(timerHandler, timeout! + 1000)
-          }
-
-          break
-        case 'CHILD:RESULT':
-          stepProcess.kill()
-          clearTimeout(timer)
-          resolve({step: msg.result.step, fatal: false, errors: msg.result.step.errors})
-          break
-        case 'CHILD:ERROR':
-          stepProcess.kill()
-          clearTimeout(timer)
-          resolve({
-            step: {
-              ...step,
-              state: PipelineState.Failed,
-            },
-            fatal: true,
-            errors: [msg.error],
-          })
-      }
-    })
-
-    stepProcess.stdout?.on('data', (chunk) => {
+    this.process.stdout?.on('data', (chunk: Buffer) => {
       const msg = chunk.toString().trim()
-      if (msg) {
-        log(msg, 'user', context, step)
-      }
+      if (msg) log(msg, 'user', this.context, this.step)
     })
 
-    stepProcess.stderr?.on('data', (chunk) => {
+    this.process.stderr?.on('data', (chunk: Buffer) => {
       const msg = chunk.toString().trim()
-      if (msg) {
-        log(msg, 'error', context, step)
-      }
+      if (msg) log(msg, 'error', this.context, this.step)
     })
 
     process.on('exit', () => {
-      stepProcess.kill()
+      this.process.kill()
     })
+  }
 
-    stepProcess.send({
-      type: 'START',
-      step: {
-        index: idx,
-        ...step,
-      },
-      context,
+  run(prefix: string = 'CHILD'): Promise<{step: any; fatal: boolean; errors: any[]}> {
+    return new Promise((resolve) => {
+      let timer: NodeJS.Timeout | null = null
+
+      const timerHandler = () => {
+        this.process.kill()
+        const error = new WorkflowError('hard timeout limit reached', WorkflowErrorCode.HardTimeout)
+        const updated = {
+          ...this.step,
+          startedAt: new Date(this.startedAt).toISOString(),
+          endedAt: new Date().toISOString(),
+          duration: Date.now() - this.startedAt,
+          state: PipelineState.Failed,
+          error,
+          errors: [error],
+        }
+
+        event(WorkflowEvent.StepFailed, {
+          step: updated,
+          attempt: {error: updated.error, retries: 0, nextMs: 0, finalAttempt: true},
+        })
+
+        resolve({step: updated, fatal: true, errors: []})
+      }
+
+      if (this.timeoutMs) {
+        timer = setTimeout(timerHandler, this.timeoutMs)
+      }
+
+      this.process.on('message', (msg: any) => {
+        switch (msg.type) {
+          case `${prefix}:EVENT`:
+            event(msg.event, msg.payload)
+
+            if (msg.event === WorkflowEvent.StepRetry) {
+              if (timer) clearTimeout(timer)
+              timer = setTimeout(timerHandler, this.timeoutMs! + msg.payload.attempt.nextMs + 500)
+            }
+
+            if (msg.event === WorkflowEvent.StepStarted) {
+              if (timer) clearTimeout(timer)
+              timer = setTimeout(timerHandler, this.timeoutMs! + 1000)
+            }
+            break
+
+          case `${prefix}:RESULT`:
+            this.process.kill()
+            if (timer) clearTimeout(timer)
+            resolve({step: msg.result.step, fatal: false, errors: msg.result.step.errors})
+            break
+
+          case `${prefix}:ERROR`:
+            this.process.kill()
+            if (timer) clearTimeout(timer)
+            resolve({
+              step: {...this.step, state: PipelineState.Failed},
+              fatal: true,
+              errors: [msg.error],
+            })
+            break
+        }
+      })
+
+      // send start command
+      this.process.send({type: 'START', step: this.step, context: this.context})
     })
+  }
+}
+
+async function runStep(idx: number, step: PipelineStep, context: WorkflowContext) {
+  const startedAt = new Date().toISOString()
+  let timeoutMs: number | undefined
+  if (step.options?.timeout) {
+    timeoutMs =
+      typeof step.options.timeout === 'string' ? ms(step.options.timeout as ms.StringValue) : step.options.timeout
+  }
+
+  const envResolved = resolveStepData(step.env || {}, {
+    context,
+    step,
   })
+
+  step.env = envResolved as Record<string, string>
+
+  const path = join(__dirname, 'workflow.step-process.js')
+  const manager = new ProcessManager({...step, index: idx, startedAt}, context, path, timeoutMs)
+  manager.fork()
+  return manager.run()
 }
 
 process.on('message', async (msg: ParentMessage<SourceData>) => {
