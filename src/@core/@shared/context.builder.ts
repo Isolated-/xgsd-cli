8f6a15b912643afe0cdad186335b875a2b892469
Michael Palmer <michaelpalmer53@gmail.com>
2025-09-04 22:30:49 +0100

    context has been updated to include cli version, if it is docker or not and a config hash

--- src/@core/@shared/context.builder.ts
+++ src/@core/@shared/context.builder.ts
@@ -1,9 +1,15 @@
 import {v4} from 'uuid'
 import {FlexibleWorkflowConfig, PipelineState, PipelineStep, SourceData} from '../@types/pipeline.types'
 import {EventEmitter2} from 'eventemitter2'
+import {createHash} from 'crypto'
+import {pathExistsSync} from 'fs-extra'
 
 export class WorkflowContext<T extends SourceData = SourceData> {
   id: string
+  version: string
+  hash: string
+  cli: string
+  docker: boolean
   stream: EventEmitter2
   runner: string
   mode: 'chained' | 'async' | 'fanout'
@@ -18,8 +24,12 @@ export class WorkflowContext<T extends SourceData = SourceData> {
   config: FlexibleWorkflowConfig<T>
   steps: PipelineStep<T>[]
 
-  constructor(config: FlexibleWorkflowConfig<T>, event: EventEmitter2) {
+  constructor(config: FlexibleWorkflowConfig<T>, event: EventEmitter2, cli?: string) {
     this.id = v4()
+    this.hash = createHash('sha256').update(JSON.stringify(config)).digest('hex').slice(0, 8)
+    this.cli = cli || 'unknown'
+    this.docker = pathExistsSync('/.dockerenv')
+    this.version = 'v' + config.version
     this.stream = event || new EventEmitter2()
     this.runner = config.runner
     this.name = config.name || config.package?.split('/').pop()!
