60d0387af98d8b49b18528f989630ee8de5b66ad
Michael Palmer <michaelpalmer53@gmail.com>
2025-09-05 05:52:05 +0100

    removes unneeded dependencies

--- src/@core/keys/interfaces/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './key-opts.interface'
-export * from './key.interface'
-export * from './keystore.interface'
--- src/@core/keys/interfaces/keystore.interface.ts
+++
@@ -1,9 +0,0 @@
-import {IKeyOpts} from './key-opts.interface'
-import {IKey} from './key.interface'
-
-export interface IKeyStore {
-  load(context: string, opts?: IKeyOpts): Promise<IKey>
-  save(key: IKey, opts?: IKeyOpts): Promise<void>
-  exists(opts?: IKeyOpts): Promise<boolean>
-  delete(opts?: IKeyOpts): Promise<void>
-}
