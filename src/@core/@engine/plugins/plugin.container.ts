ce014e79edea5e3e1f624f76b53193af213edf59
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-18 20:14:39 +0100

    uses resolveFactory in PluginContainer.use()

--- src/@core/@engine/plugins/plugin.container.ts
+++ src/@core/@engine/plugins/plugin.container.ts
@@ -1,6 +1,7 @@
 import {PluginFactory, PluginInput} from '../types/factory.types'
 import {Hooks} from '../types/hooks.types'
 import {ProjectConfig, ProjectContext} from '../types/project.types'
+import {resolveFactory} from '../util'
 
 export class PluginContainer {
   public readonly config: ProjectConfig
@@ -12,18 +13,7 @@ export class PluginContainer {
   private factories: ((ctx: ProjectContext) => Hooks)[] = []
 
   use(input: PluginInput) {
-    if (typeof input === 'function') {
-      this.factories.push((ctx) => {
-        try {
-          return new (input as any)(ctx)
-        } catch {
-          return (input as PluginFactory)(ctx)
-        }
-      })
-      return
-    }
-
-    this.factories.push(() => input)
+    this.factories.push(resolveFactory(input))
   }
 
   createHooks(context: ProjectContext): Hooks[] {
