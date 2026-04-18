239de1be737d57293cd6d53572b61d95fe4b709e
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-18 20:23:22 +0100

    cleans up pluginregistry + util fns

--- src/@core/@engine/plugins/plugin.container.ts
+++ src/@core/@engine/plugins/plugin.container.ts
@@ -12,7 +12,7 @@ export class PluginRegistry implements Registry<PluginInput, Hooks[]> {
     this.config = context.config
   }
 
-  private factories: ((ctx: ProjectContext) => Hooks)[] = []
+  private factories: PluginFactory[] = []
 
   use(input: PluginInput) {
     this.factories.push(resolveFactory(input))
@@ -20,20 +20,5 @@ export class PluginRegistry implements Registry<PluginInput, Hooks[]> {
 
   build(ctx: WorkflowContext<unknown>): Hooks[] {
     return buildFactories(this.factories, ctx)
-  }
-
-  createHooks(context: ProjectContext): Hooks[] {
-    // this fixes user errors like:
-    // xgsd.use((ctx) => {}) (no returns)
-    // by dropping the plugin before its registered
-    return this.factories
-      .map((f) => {
-        try {
-          return f(context)
-        } catch {
-          return undefined
-        }
-      })
-      .filter((hook): hook is Hooks => !!hook)
   }
 }
--- src/@core/@engine/plugins/plugin.util.ts
+++ src/@core/@engine/plugins/plugin.util.ts
@@ -1,55 +1,7 @@
 import {Hooks} from '../types/hooks.types'
 import {Block} from '../types/block.types'
 import {ProjectContext} from '../types/project.types'
-import {PluginRegistry} from './plugin.container'
-import {PluginManager} from './plugin.manager'
 import {RetryAttempt} from '../types/retry.types'
-import {SetupContainer} from '../setup'
-import {PluginInput} from '../types/factory.types'
-
-export const loadUserPlugins = (context: ProjectContext, container: PluginRegistry) => {
-  const mod = require(context.package)
-
-  if (typeof mod.plugins === 'function') {
-    mod.plugins(container)
-  }
-}
-
-export const loadUserSetup = async (context: ProjectContext, setup: SetupContainer) => {
-  const userModule = await import(context.package)
-
-  if (typeof userModule.setup === 'function') {
-    await userModule.setup(setup)
-  }
-}
-
-export const createRuntime = async (context: ProjectContext, plugins?: PluginInput[]) => {
-  const setup = new SetupContainer(context)
-
-  plugins?.forEach((plugin) => setup.use(plugin))
-
-  await loadUserSetup(context, setup)
-
-  const {pluginManager, executor} = setup.build(context)
-
-  return {pluginManager, executor}
-}
-
-export const createPluginManager = (context: ProjectContext, plugins?: PluginInput[]) => {
-  const container = new PluginRegistry(context)
-
-  // register plugins
-  plugins?.forEach((plugin) => container.use(plugin))
-
-  // user plugins
-  //loadUserPlugins(context.format!() as any, container)
-
-  const hooks = container.createHooks(context)
-
-  const manager = new PluginManager(hooks)
-
-  return manager
-}
 
 const ctxOnly = (ctx: ProjectContext) => [ctx]
 const ctxBlock = (ctx: ProjectContext, block?: Block) => [ctx, block]
