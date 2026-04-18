c72768a1881e9fa097f2e7d85d30bf845b4aa6fb
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-18 20:25:14 +0100

    moves invokeFn from plugin.util.ts

--- src/@core/@engine/util.ts
+++ src/@core/@engine/util.ts
@@ -3,7 +3,9 @@ import {WorkflowError, WorkflowErrorCode} from './error'
 import {SetupContainer} from './setup'
 import {Block} from './types/block.types'
 import {ExecutorInput, Factory, FactoryInput, PluginInput} from './types/factory.types'
+import {Hooks} from './types/hooks.types'
 import {ProjectContext} from './types/project.types'
+import {RetryAttempt} from './types/retry.types'
 
 export type UserSetupFn = (ctx: WorkflowContext, setup: SetupContainer) => Promise<void>
 
@@ -86,4 +88,40 @@ export const createRuntime = async (opts: {
   await userCodeFn(context, setup)
 
   return setup.build(context)
+}
+
+const ctxOnly = (ctx: ProjectContext) => [ctx]
+const ctxBlock = (ctx: ProjectContext, block?: Block) => [ctx, block]
+
+const INVOKE_ARGS = {
+  projectStart: ctxOnly,
+  projectEnd: ctxOnly,
+
+  blockStart: ctxBlock,
+  blockEnd: ctxBlock,
+  blockWait: ctxBlock,
+  blockSkip: ctxBlock,
+
+  blockRetry: (ctx: ProjectContext, block?: Block, attempt?: RetryAttempt) => [ctx, block, attempt],
+} as const
+
+export type InvokeFn = keyof typeof INVOKE_ARGS
+
+export const invoke = async (
+  hooks: Hooks[],
+  fn: InvokeFn,
+  context: ProjectContext,
+  block?: Block,
+  attempt?: RetryAttempt,
+): Promise<void> => {
+  for (const hook of hooks) {
+    const method = hook[fn]
+
+    if (typeof method !== 'function') continue
+
+    try {
+      const args = INVOKE_ARGS[fn](context, block, attempt)
+      await (method as any).call(hook, ...args)
+    } catch (error) {}
+  }
 }
