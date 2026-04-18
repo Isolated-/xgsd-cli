9f0177bf64f508fc6e073a531b0d2d33116383ff
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-18 20:18:21 +0100

    implements Registry interface in PluginContainer

--- src/@core/@engine/plugins/plugin.container.ts
+++ src/@core/@engine/plugins/plugin.container.ts
@@ -1,9 +1,11 @@
+import {WorkflowContext} from '../context.builder'
 import {PluginFactory, PluginInput} from '../types/factory.types'
 import {Hooks} from '../types/hooks.types'
+import {Registry} from '../types/interfaces/registry.interface'
 import {ProjectConfig, ProjectContext} from '../types/project.types'
-import {resolveFactory} from '../util'
+import {buildFactories, resolveFactory} from '../util'
 
-export class PluginContainer {
+export class PluginContainer implements Registry<PluginInput, Hooks[]> {
   public readonly config: ProjectConfig
 
   constructor(context: ProjectContext) {
@@ -14,6 +16,10 @@ export class PluginContainer {
 
   use(input: PluginInput) {
     this.factories.push(resolveFactory(input))
+  }
+
+  build(ctx: WorkflowContext<unknown>): Hooks[] {
+    return buildFactories(this.factories, ctx)
   }
 
   createHooks(context: ProjectContext): Hooks[] {
