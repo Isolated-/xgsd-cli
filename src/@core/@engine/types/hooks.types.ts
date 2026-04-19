ee957d41810d75da7c2d28fa7c3a53bc53cb7ba2
Michael Palmer <michaelpalmer53@gmail.com>
2026-04-19 19:12:23 +0100

    updates Hooks interface to be more flexible, exposing on(), init(), and exit()

--- src/@core/@engine/types/hooks.types.ts
+++ src/@core/@engine/types/hooks.types.ts
@@ -5,6 +5,19 @@ import {RetryAttempt} from './retry.types'
 export type HookType = ProjectContext | Block | RetryAttempt
 
 export interface Hooks {
+  // new event handler
+  // generic to avoid need to add more events
+  // like projectWait
+  on?<T = unknown>(event: string, payload: T): Promise<void> | void
+
+  // init()
+  // called when extension is first loaded
+  init?(context: ProjectContext): Promise<void> | void
+
+  // exit()
+  // called when extension is unloaded
+  exit?(context: ProjectContext): Promise<void> | void
+
   // project
   projectStart?(context: ProjectContext): Promise<void>
   projectEnd?(context: ProjectContext): Promise<void>
