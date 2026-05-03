8188a780bb5380016b56eef00d34d5dbbd147084
Michael Palmer <michaelpalmer53@gmail.com>
2026-05-04 00:51:45 +0100

    brings UserHooksPlugin back into @xgsd/cli

--- src/presets/default.preset.ts
+++ src/presets/default.preset.ts
@@ -2,10 +2,11 @@ import {DefaultOrchestrator, ProcessExecutor, RuntimePreset} from '@xgsd/runtime
 import {UserLogsPlugin} from '../plugins/user-logs.plugin'
 import {MetricsPlugin} from '../plugins/metrics.plugin'
 import {ReporterPlugin} from '../plugins/reporter.plugin'
+import {UserHooksPlugin} from '../plugins/userhooks.plugin'
 
 export function defaultPreset(opts: any): Partial<RuntimePreset> {
   return {
-    plugins: [new UserLogsPlugin(), new MetricsPlugin(opts), new ReporterPlugin(opts)],
+    plugins: [new UserLogsPlugin(), new UserHooksPlugin(opts), new MetricsPlugin(opts), new ReporterPlugin(opts)],
     executor: ProcessExecutor,
     orchestrator: DefaultOrchestrator,
   }
